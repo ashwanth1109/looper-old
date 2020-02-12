7c9de057a277b707f716c9b3ef471faacba155ff
Ashwanth A R <ashwanth1109@gmail.com>
2020-02-13 01:39:42 +0530

    Basic Video Player functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useRef, useEffect } from 'react';
 
-export default () => (
+let loadYT;
+export default () => {
+  const playerRef = useRef(null);
+  let player;
+  useEffect(() => {
+    if (!loadYT) {
+      loadYT = new Promise(resolve => {
+        const tag = document.createElement('script');
+        tag.src = 'https://www.youtube.com/iframe_api';
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        window.onYouTubeIframeAPIReady = () => resolve(window.YT);
+      });
+    }
+
+    let done = false;
+    const stopVideo = () => {
+      player.stopVideo();
+    };
+    const onPlayerStateChange = e => {
+      console.log('State was changed');
+      if (e.data === YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+      }
+    };
+
+    const onPlayerReady = e => {
+      e.target.playVideo();
+    };
+
+    loadYT.then(YT => {
+      player = new YT.Player(playerRef.current, {
+        height: 390,
+        width: 640,
+        videoId: '4y33h81phKU',
+        events: {
+          onStateChange: onPlayerStateChange,
+          onReady: onPlayerReady
+        }
+      });
+    });
+  }, []);
+
+  return (
     <>
-        <h1>
-            Welcome to Create React Simple App!
-        </h1>
-        <h3>📦 Build with Parcel</h3>
-        <h3>🚀 Deploy to Github Pages</h3>
+      <h1>Looper POC</h1>
+      <div ref={playerRef}></div>
     </>
-);
+  );
+};
